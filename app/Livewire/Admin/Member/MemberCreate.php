7958420b2e9e62007959c968a8824c0cbce7c903
Sabilidn <mshidiq290@gmail.com>
2025-12-19 07:06:06 +0300

    third commit

--- app/Livewire/Admin/Member/MemberCreate.php
+++ app/Livewire/Admin/Member/MemberCreate.php
@@ -7,11 +7,13 @@ use Livewire\Component;
 
 class MemberCreate extends Component
 {
-    public $nama, $nomor_handphone;
+    public $nama, $nomor_handphone, $plat_nomor, $jenis_kendaraan;
 
     protected $rules = [
         'nama' => 'required|string|max:100|unique:members,nama',
-        'nomor_handphone' => 'required|max:15'
+        'nomor_handphone' => 'required|max:15',
+        'plat_nomor' => 'required|max:15',
+        'jenis_kendaraan' => 'required|max:20',
     ];
 
     public function save()
@@ -21,7 +23,9 @@ class MemberCreate extends Component
 
             Member::create([
                 'nama' => $this->nama,
-                'nomor_handphone' => $this->nomor_handphone
+                'nomor_handphone' => $this->nomor_handphone,
+                'plat_nomor' => $this->plat_nomor,
+                'jenis_kendaraan' => $this->jenis_kendaraan,
             ]);
 
             toastr()->success('Berhasil Tambah Data');
